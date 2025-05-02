56e99b9973fa1d1f03d7dc0b7f7788e9ccb3b071
Youssef Alaa <youssefalaacj@gmail.com>
2025-05-02 03:21:19 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kidzoo/modules/FlappyBird/flappy_bird_game.dart';
-import 'package:kidzoo/modules/home/UI/home_screen.dart';
+
+import 'modules/Alphabets/bloc/alphabet_bloc.dart';
+import 'modules/home/UI/character.dart';
+import 'modules/home/UI/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +23,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: GameWidget(game: FlappyBirdGame()),
+         home: BlocProvider(
+        create: (context) => AlphabetBloc(),
+        child: const CharacterSelectionScreen(),
+      ),
+          // CharacterSelectionScreen(),
+      // GameWidget(game: FlappyBirdGame()),
       // BlocProvider(
       //   create: (context) => AlphabetBloc(),
       //   child: const HomeScreen(),
